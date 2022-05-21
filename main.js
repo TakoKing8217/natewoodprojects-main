0cf79e8ed10f21d458e1a8f77653a50bb0cdd752
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-05-21 08:55:36 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("Speed Racer");
+
+let geetBtn = document.querySelector("#greeting");
+
+geetBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  alert("Hey Babe! Lookin hot today!");
+});
